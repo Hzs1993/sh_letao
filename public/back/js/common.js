8b6e4c2df9cd000032febf26a624602c68ed2783
hongzongli <1667843192@qq.com>
2018-06-25 23:10:00 +0800

    首页部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,11 +1,34 @@
 /**
  * Created by hong on 2018/6/25.
  */
+
+
+if(location.href.indexOf("login.html")===-1){
+
+  $.ajax({
+    url:"/employee/checkRootLogin",
+    dateType:"json",
+    type:"get",
+    success:function (info) {
+      console.log(info);
+      if(info.error===400){
+          location.href="login.html"
+        }
+      if(info.success){
+
+      }
+    }
+  })
+}
+
+
+
+
+
+
 $(document).ajaxStart(function () {
     NProgress.start();
 })
-
-
 
 
 $(document).ajaxStop(function () {
@@ -14,5 +37,57 @@ $(document).ajaxStop(function () {
   },1000)
 
 })
+
+
+//公共功能
+
+$(function(){
+
+  $('.lt_aside .category').click(function() {
+    $('.lt_aside .child').stop().slideToggle();
+  });
+
+  $('.lt_topbar .icon_menu').click(function() {
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+  });
+
+
+  $('.lt_topbar .icon_logout').click(function () {
+      $('#logoutModal').modal('show')
+      $('#logoutBtn').click(function () {
+          $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function (info) {
+                if(info.success){
+                  location.href='login.html'
+                }
+            }
+
+          })
+      })
+
+
+
+  })
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
